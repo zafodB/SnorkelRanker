d2e9853320c44d3f7d826e704b4b0468c1103020
filip <filiplama@gmail.com>
2019-11-26 15:37:10 +0100

    Finish the metrics

---
+++ calculate_stats.py
@@ -0,0 +1,15 @@
+'''
+ * Created by filip on 26/11/2019
+'''
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df_valid = pd.read_csv("/filip/json/ehealthforum/trac/validation_df.txt", sep="\t", header=0)
+
+print(list(df_valid.columns))
+
+df_valid['predicted_prob'].plot.hist(bins=10)
+plt.savefig("/filip/json/ehealthforum/trac/plot.png")
+
+print(df_valid.head())
--- run_snorkel_classifier.py
+++ run_snorkel_classifier.py
@@ -13,13 +13,13 @@ from snorkel.labeling import labeling_function, PandasLFApplier, LFAnalysis, Lab
 print("Hello there docker!")
 
 METRIC_AT = 5
-PROBABILITY_CUTOFF = 0.9
+PROBABILITY_CUTOFF = 0.1
 
 # df = pd.read_csv("/filip/json/ehealthforum/trac/training_data.txt", sep='\t',
 #                  names=['mdreply', 'votesh', 'votess', 'votest', 'ment', 'sameent', 'length', 'category', 'thread'])
 
 df = pd.read_csv("/filip/json/ehealthforum/trac/training_data_snorkel_10k_titles.txt", sep='\t', header=0,
-                 error_bad_lines=False, encoding="ISO-8859–1")
+                 error_bad_lines=True, encoding="ISO-8859–1")
 
 
 def split_by_char(input_text, character=';'):
@@ -33,20 +33,19 @@ df['document_annotations'] = df['document_annotations'].apply(split_by_char)
 df['query_annotations'] = df['query_annotations'].apply(split_by_char)
 df.relationships_list = df.relationships_list.apply(split_by_char, args=(',',))
 
-# print(df['document_annotations'].head())
+df = df.sort_values(['query_thread', 'bm25_score'], ascending=[True, False])
 
-# print(list(df.columns))
 # Keep just a bit of data for development for now
-# df = df.iloc[:500,]
-print(df.relationships_list.head())
+# df = df.iloc[:1000, ]
+print(df.head())
 # print()
-print(df.info())
-print(df.describe())
+# print(df.bm25_score.info())
+print(df.bm25_score.describe())
 # print()
 # print(df.shape)
 # print(df.ndim)
 
-
+# region Labeling functions
 # Define the label mappings for convenience
 ABSTAIN = -1
 NOT_RELEVANT = 0
@@ -181,9 +180,11 @@ def has_type_inpo(x):
 def has_type_elii(x):
     return RELEVANT if x.d_typ_elii > 0 else ABSTAIN
 
+
 @labeling_function()
 def has_type_diap_medd_or_bhvr(x):
     return RELEVANT if x.d_typ_diap > 0 or x.d_typ_medd > 0 or x.d_typ_bhvr > 0 else ABSTAIN
+
 
 @labeling_function()
 def number_relations_total(x):
@@ -194,13 +195,18 @@ def number_relations_total(x):
 def number_relations_distinct(x):
     entities = set(x.relationships_list)
     # if 'isa' in entities:
-        # entities.remove('isa')
+    # entities.remove('isa')
     return RELEVANT if len(entities) > 0 else NOT_RELEVANT
 
 
 @labeling_function()
 def has_treatment(x):
     return RELEVANT if 'treats' in x.relationships_list or 'indicates' in x.relationships_list else ABSTAIN
+
+
+# endregion
+
+
 '''
 ['query_category', 'query_thread', 'query_text', 'query_annotations', 'typ_dsyn', 'typ_patf', 'typ_sosy', 'typ_dora', 
 'typ_fndg', 'typ_menp', 'typ_chem', 'typ_orch', 'typ_horm', 'typ_phsu', 'typ_medd', 'typ_bhvr', 'typ_diap', 'typ_bacs',
@@ -232,12 +238,76 @@ def has_treatment(x):
 '''
 
 
-def classify_my_probs(x):
-    return 1 if x < PROBABILITY_CUTOFF else 0
+def apk(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+    This function computes the average prescision at k between two lists of
+    items.
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+    """
+    if len(predicted) > k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i, p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i + 1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
+
+
+def mapk(actual, predicted, k=10):
+    """
+    Computes the mean average precision at k.
+    This function computes the mean average prescision at k between two lists
+    of lists of items.
+    Parameters
+    ----------
+    actual : list
+             A list of lists of elements that are to be predicted
+             (order doesn't matter in the lists)
+    predicted : list
+                A list of lists of predicted elements
+                (order matters in the lists)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The mean average precision at k over the input lists
+    """
+    return np.mean([apk(a, p, k) for a, p in zip(actual, predicted)])
+
+
+def classify_my_probs(x, cutoff):
+    return 1 if x > cutoff else 0
 
 
 # print(df.document_user_status.unique())
-df_train, df_valid = train_test_split(df, test_size=0.1, random_state=8886, stratify=df.bm25_relevant)
+# df_train, df_valid = train_test_split(df, test_size=0.1, random_state=8886, stratify=df.bm25_relevant)
+# df_train, df_valid = train_test_split(df, test_size=0.5, shuffle=False)
+# df_train, df_valid = np.array_split(df, 2)
+df_valid = df.iloc[:10000, ]
+df_train = df.iloc[10000:, ]
+
+
 Y_valid = df_valid.bm25_relevant.values
 Y_train = df_train.bm25_relevant.values
 
@@ -252,44 +322,65 @@ lfs = [has_votes, is_doctor_reply, is_same_thread, has_entities, number_relation
 
 applier = PandasLFApplier(lfs=lfs)
 
-# print(df_train.loc[23467])
-# print(df_train.sample(5))
-
-#
-# L_dev = applier.apply(df=df_dev)
 L_train = applier.apply(df=df_train)
 L_valid = applier.apply(df=df_valid)
-#
-print(L_train)
-# #
+
 print(LFAnalysis(L=L_train, lfs=lfs).lf_summary(Y=Y_train))
 
 label_model = LabelModel(cardinality=2, verbose=True)
-label_model.fit(L_train=L_train, n_epochs=20000, lr=0.0001, log_freq=10, seed=81794)
+label_model.fit(L_train=L_train, n_epochs=20000, lr=0.0001, log_freq=10, seed=2345)
 # label_model.fit(L_train=L_train, n_epochs=20, lr=0.0001, log_freq=10, seed=81794)
 
+print("Model weights: " + str(label_model.get_weights()))
+
 valid_probabilities = label_model.predict_proba(L=L_valid)
-# validation_labels = np.fromfunction(classify_my_probs, valid_probabilities)
-validation_labels = np.fromiter((classify_my_probs(probab[1]) for probab in valid_probabilities), int)
+df_valid.insert(50, 'predicted_prob', valid_probabilities[:, 1])
 
-print(average_precision_score(Y_valid, np.transpose(valid_probabilities)[0]))
-# joined_vals = np.column_stack((validation_labels, Y_valid))
-joined_vals = np.array([validation_labels, Y_valid])
-joined_vals = np.vstack((joined_vals, np.zeros_like(joined_vals[0])))
-
-for index, value in enumerate(joined_vals[2]):
-    joined_vals[2, index] = index % 20
-
-joined_vals2 = joined_vals[:, joined_vals[2] < METRIC_AT]
+df_valid.to_csv("/filip/json/ehealthforum/trac/validation_df.txt", sep="\t", header=True)
+# df_valid['predicted_prob'].plot.hist(bins=10)
+# plt.savefig("/filip/json/ehealthforum/trac/plot.png")
 
 
-print("Accuracy: " + str(accuracy_score(joined_vals[0], joined_vals[1])))
-print("Precision: " + str(precision_score(joined_vals2[0], joined_vals2[1])))
+for cutoff in range(1, 10):
+    PROBABILITY_CUTOFF = (cutoff / 10)
+    validation_labels = np.fromiter((classify_my_probs(probab[1], PROBABILITY_CUTOFF) for probab in valid_probabilities), int)
 
+    if 'predicted_label' in df_valid.columns:
+        df_valid = df_valid.drop(columns=['predicted_label'])
+    df_valid.insert(51, 'predicted_label', validation_labels)
+    print("Unique values: " + str(df_valid['predicted_prob'].nunique()))
 
-label_model_acc = label_model.score(L=L_valid, Y=Y_valid)["accuracy"]
-print(f"{'Label Model Accuracy:':<25} {label_model_acc * 100:.1f}%")
-print(label_model.get_weights())
+    mapk_true = (df_valid.sort_values(['query_thread', 'bm25_score'], ascending=[True, False])
+                 .groupby('query_thread')
+                 .head(10)
+                 .groupby('query_thread')['document_text']
+                 .apply(list)
+                 .tolist())
+
+    mapk_predicted = (df_valid.sort_values(['query_thread', 'predicted_prob'], ascending=[True, False])
+                      .groupby('query_thread')
+                      .head(10)
+                      .groupby('query_thread')['document_text']
+                      .apply(list)
+                      .tolist())
+
+    df_valid_reduced = df_valid.sort_values(['query_thread', 'predicted_prob'], ascending=[True, False]).groupby(
+        'query_text').head(METRIC_AT)
+
+    print("\nCutoff value (labelled True if above): " + str(PROBABILITY_CUTOFF) + '\nMetrics@: ' + str(METRIC_AT) + '\n')
+
+    # print(average_precision_score(Y_valid, np.transpose(valid_probabilities)[1]))
+
+    print("Number of True relevant: " + str(df_valid_reduced[df_valid_reduced.bm25_relevant == 1].count()['bm25_relevant']))
+    print("Number of Predicted relevant: " + str(
+        df_valid_reduced[df_valid_reduced.predicted_label == 1].count()['predicted_label']) + '\n')
+
+    print("Accuracy: " + str(accuracy_score(df_valid_reduced.bm25_relevant, df_valid_reduced.predicted_label)))
+    print("Precision: " + str(precision_score(df_valid_reduced.bm25_relevant, df_valid_reduced.predicted_label)))
+    print("MAP@"+str(METRIC_AT) + ': ' + str(mapk(mapk_true, mapk_predicted, METRIC_AT)))
+
+# label_model_acc = label_model.score(L=L_valid, Y=Y_valid)["accuracy"]
+# print(f"{'Label Model Accuracy:':<25} {label_model_acc * 100:.1f}%")
 
 
 print('\nsup')
